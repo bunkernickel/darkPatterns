658ade0f9ec7edc9e058f9c6527c7067b281e487
bunkernickel <cbizjak21@jcu.edu>
2022-04-26 21:36:54 -0400

    Final Joke Actually

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I'm gay and my dick is small
+          Some worrying news
         </a>
       </header>
     </div>
